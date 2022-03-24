27f9352a7a011899bf766f43e73e39ffcb657c0b
mayoyooyo <ittiwat.35131@gmail.com>
2022-03-24 15:14:37 +0700

    crate deatil component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import logo from './logo.svg'
-import './App.css'
-import { Button } from 'semantic-ui-react'
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "semantic-ui-react";
+import Details from "./components/Details";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Button secondary>dwd</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+      <Details />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
