10a84dcf159c5b1e2a285aef753e84ec1b014c66
JAmoMES <atipat.pa@ku.th>
2022-03-24 21:39:03 +0700

    add leaflet map

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import { Image } from 'semantic-ui-react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+const position = [51.505, -0.09]
 
 const Home = () => {
   return (
-    <div>
-      {/* <Image src='https://i.dlpng.com/static/png/6838099_preview.png' fluid /> */}
-    </div>
+    <MapContainer center={position} zoom={13}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+      />
+      <Marker position={position}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
